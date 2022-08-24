4b517dde1fac93307863488093e32b821788a1f5
vladimir <grun_gespenst@tut.by>
2022-08-24 15:45:33 +0300

    init added

--- opp.py
+++ opp.py
@@ -1,15 +1,22 @@
+from typing.io import IO
+
+
 class Parser:
     """
     Base class for parsing
     """
+    def __init__(self, input='test_ling', lang_1='English', lang_2='Russian'):
+        self.input = input
+        self.lang_1 = lang_1
+        self.lang_2 = lang_2
 
-    def make_list(self, var: str, language: int):
+    def make_list(self, var: str, language: int) -> list:
         '''
         Makes a sheet of one language
         '''
         return var.split('\t')[language].split(';')
 
-    def write_info(self, file, word: str):
+    def write_info(self, file: IO, word: str):
         '''
         Writes information to a file
         '''
@@ -19,17 +26,17 @@ class Parser:
         '''
         Main function
         '''
-        with open('test_ling.txt', 'r', encoding='utf-8') as input, \
-                open('English.txt', 'a', encoding='utf-8') as english, \
-                open('Russian.txt', 'a', encoding='utf-8') as russian:
+        with open(f'{self.input}.txt', 'r', encoding='utf-8') as input, \
+                open(f'{self.lang_1}.txt', 'a', encoding='utf-8') as language_1, \
+                open(f'{self.lang_2}.txt', 'a', encoding='utf-8') as language_2:
             data = input.readlines()
             for string in data:
                 english_words = self.make_list(string, 0)
                 russian_words = self.make_list(string, 1)
                 for eng_word in english_words:
                     for rus_word in russian_words:
-                        self.write_info(english, eng_word)
-                        self.write_info(russian, rus_word)
+                        self.write_info(language_1, eng_word)
+                        self.write_info(language_2, rus_word)
 
 
 isinstance = Parser()
